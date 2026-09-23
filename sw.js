a32663fe422bf94b8b6081048f21f3e1556f5ac8
Noskarant <tutomod@gmail.com>
2026-09-23 10:48:37 +0200

    Activate MyWeather updates immediately

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'myweather-v1.3.1';
+const CACHE = 'myweather-v1.3.2';
 const ASSETS = ['./','./index.html','./styles.css','./js/app.js','./js/weather.js','./js/route.js','./js/utils.js','./assets/favicon.svg','./manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then(response => {
