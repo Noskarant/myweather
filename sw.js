8bb0bbb649487d358c52bb775e3fcfe1485aab68
Noskarant <tutomod@gmail.com>
2026-09-26 19:13:12 +0200

    Refresh desktop style cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myweather-v1.6.5';
+const CACHE = 'myweather-v1.6.6';
 const ASSETS = ['./','./index.html','./styles.css','./js/app.js','./js/weather.js','./js/route.js','./js/utils.js','./assets/favicon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
