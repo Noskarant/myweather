b081dafcd98c79169652c93865dc4e09aaa90aa0
Noskarant <tutomod@gmail.com>
2026-09-26 16:25:07 +0200

    Refresh app cache without intercepting map tiles

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'myweather-v1.5.9';
+const CACHE = 'myweather-v1.6.0';
 const ASSETS = ['./','./index.html','./styles.css','./js/app.js','./js/weather.js','./js/route.js','./js/utils.js','./assets/favicon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
   event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
